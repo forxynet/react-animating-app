2d34335ec7bc7c0e57e4a8d9f4284982064841b2
hakan.basturknewinn.com <forxynet@gmail.com>
2024-10-22 12:51:30 +0300

    update modal for image

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,13 +2,18 @@ import { createPortal } from 'react-dom';
 import { motion } from 'framer-motion';
 
 export default function Modal({ title, children, onClose }) {
+  
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-        initial={{opacity: 0, y: 150}}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{opacity: 0, y: 150}}
+        variants={{
+          hidden: { opacity: 0, y: 50 },
+          visible: { opacity: 1, y: 0 }
+        }}
+        initial='hidden'
+        animate='visible'
+        exit='hidden'
         open
         className="modal">
         <h2>{title}</h2>
